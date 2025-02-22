3b6cf9bcd5ae8bc197bfb9726edb46ea33f4126f
artes26 <ayush16r@gmail.com>
2025-02-22 10:46:33 +0530

    new change

---
+++ calculator.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class calculator {
+
+    public static int addSimple ( int x, int y) {
+        return x+y;
+
+    }
+
+    public static int subSimple ( int x, int y) {
+        return y-x;
+
+    }
+
+    
+    public static int multiSimple ( int x, int y) {
+        return x*y;
+
+    }
+
+    public static int divideSimple ( int x, int y) {
+        return y/x;
+
+    }
+
+    public static int remendSimple ( int x, int y) {
+        return x%y;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" enter x : ");
+        int a = sc.nextInt();
+        System.out.println(" enter y : ");
+        int b = sc.nextInt();
+
+        System.out.println(" enter the opretor : ");
+
+        char ch = Obj.nextLine().charAt(0);
+        switch (ch) {
+            case '+': System.out.println( );
+                
+                break;
+        
+            default: System.out.println( " enter the valid operator ");
+                break;
+
+            case; 
+                 
+
+        }
+
+    }
+    
+}
