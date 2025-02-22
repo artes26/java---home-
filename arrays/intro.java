109a37248eec5948589ee9112148da3bbaa319bb
artes26 <ayush16r@gmail.com>
2025-02-22 11:39:44 +0530

    arrays

---
+++ arrays/intro.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class intro {
+    
+// type [ ] arrayName = new type[size];
+// [ ] - indicates we are talking about arrays
+//  new - (  keyword )  - for taking space in memory 
+// 
+public static void main(String[] args) {
+    int [ ] marks = new int[3];
+    marks[0]= 97;
+    marks[1]= 87;
+    marks[2]= 45;
+
+    System.out.println(marks[0]);
+    
+      
+}
+
+
+}
---
+++ arrays/loopconcept.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class loopconcept {
+    public static void main(String[] args) {
+        int [ ] marks = new int[3];
+        marks[0]= 97;
+        marks[1]= 87;
+        marks[2]= 45;
+    
+        //System.out.println(marks[0]);
+        for (int i = 0; i <3; i++) {
+            System.out.println(marks[i]);
+            
+        }
+        
+          
+    }
+
+}
