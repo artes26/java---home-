9aeb24376b2d144107fd08c3e8d9fb033f92aa72
artes26 <ayush16r@gmail.com>
2025-05-14 01:23:01 +0530

    factorial revision

--- funtion/printFactorial.java
+++ funtion/printFactorial.java
@@ -1,25 +1,46 @@
 package funtion;
 import java.util.*;
-public class printFactorial { 
+// public class printFactorial { 
 
-    public static void printFacto ( int n  ) { 
-         //loop
-         int factorial = 1;
-         for ( int i=n; i>=1; i-- ) {
-            factorial = factorial * i;
-            }
-            System.out.println(factorial);
-        return;
-    }
+//     public static void printFacto ( int n  ) { 
+//          //loop
+//          int factorial = 1;
+//          for ( int i=n; i>=1; i-- ) {
+//             factorial = factorial * i;
+//             }
+//             System.out.println(factorial);
+//         return;
+//     }
 
  
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println(" input n : ");
+//         int n = sc.nextInt();
+//         printFacto(n);
+
+
+//     }
+    
+// }
+
+public class printFactorial {
+
+    public static void printFactorial(int n ) {
+        int factorial=1;
+        for( int i=n; i>=1; i--){
+            factorial= factorial*i;
+            
+        }
+        System.out.println(factorial);
+        return;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(" input n : ");
         int n = sc.nextInt();
-        printFacto(n);
+        printFactorial(n);
 
-
+        
     }
-    
 }
