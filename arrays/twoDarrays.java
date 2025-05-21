bdb7fd34a9c42743c5d2231b8ab7356fcb31cc65
artes26 <ayush16r@gmail.com>
2025-05-21 10:06:42 +0530

    2-d arrays concept

---
+++ arrays/twoDarrays.java
@@ -0,0 +1,36 @@
+package arrays;
+import java.util.*;
+public class twoDarrays
+{
+    
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int [][] number = new int[rows][cols];
+
+        for ( int i=0; i<rows; i++)
+        {
+            //column
+            for(int j=0; j<cols; j++){
+                number[i][j]= sc.nextInt();
+            }
+
+        }
+
+        // ouput
+        for ( int i=0; i<rows; i++)
+        {
+            //column
+            for(int j=0; j<cols; j++){
+                System.out.print(number[i][j]+ " ");
+            }
+            System.out.println();
+
+        } 
+        
+
+        
+    }
+}
